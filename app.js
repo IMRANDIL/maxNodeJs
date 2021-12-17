8b9e7fbea827a99e13d41a860ea42f3d184d6932
IMRANDIL <aliimranadil2@gmail.com>
2021-12-17 20:01:24 +0530

    yh

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const Err = require('./controllers/err');
 
 // const bodyparser = require('body-parser')
 const app = express();
-app.use(express.static(path.join(__dirname, 'public/css')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.json())
