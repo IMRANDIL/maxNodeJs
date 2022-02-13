5412ca704bb19e28a24a1bf716794c212f02a5c8
IMRANDIL <aliimranadil2@gmail.com>
2022-02-13 10:10:03 +0530

    yh

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 exports.getLogin = (req, res, next) => {
 
-
-    res.render('auth/login', { path: req.url, title: 'Login_Page', isAuthenticated: req.isLoggedIn })
+    const isLoggedIn = req.get('Cookie').trim().split('=')[1]
+    res.render('auth/login', { path: req.url, title: 'Login_Page', isAuthenticated: isLoggedIn })
 
 
 }
@@ -9,7 +9,7 @@ exports.getLogin = (req, res, next) => {
 
 exports.postLogin = (req, res, next) => {
 
-    req.isLoggedIn = true;
+    res.setHeader('Set-Cookie', 'loggedIn=true')
 
 
     res.redirect('/')
